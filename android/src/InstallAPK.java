a0b6988e5747e51d7411c5504384293995268df1
Илья <ilazukov65@gmail.com>
2025-09-04 18:35:40 +0300

    Add files via upload

--- android/src/InstallAPK.java
+++ android/src/InstallAPK.java
@@ -1,5 +1,6 @@
-import org.qtproject.qt5.android.QtNative;
 
+package android.src.InstallAPK;
+import org.qtproject.qt.android.QtNative;
 import java.lang.String;
 import java.io.File;
 import android.content.Intent;
@@ -9,28 +10,33 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.support.v4.content.FileProvider;
 
+
 public class InstallAPK
 {
-    private  Context context = this.context;
     protected InstallAPK()
     {
     }
 
-    public  int installApp(String appPackageName) {
-        if (QtNative.activity() == null)
-            return -1;
-        try {
-            File apk = new File(appPackageName);
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri uri = FileProvider.getUriForFile(context,"org.qtproject.install.fileProvider", apk);
-            intent.setDataAndType(uri,"application/vnd.android.package-archive");
+    private  Context context = this.context;
 
-            QtNative.activity().startActivity(intent);
-            return 0;
-        } catch (android.content.ActivityNotFoundException anfe) {
-            return -3;
-        }
-    }
+    public  int installApp(String appPackageName) {
+
+         if (QtNative.activity() == null)
+            return -1;
+         try {
+             File apk = new File(appPackageName);
+             Intent intent = new Intent(Intent.ACTION_VIEW);
+             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+             Uri uri = FileProvider.getUriForFile(context,"org.qtproject.install.fileProvider", apk);
+             intent.setDataAndType(uri,"application/vnd.android.package-archive");
+
+             QtNative.activity().startActivity(intent);
+
+             return 0;
+         } catch (android.content.ActivityNotFoundException anfe) {
+             return -3;
+         }
+     }
 }
+
